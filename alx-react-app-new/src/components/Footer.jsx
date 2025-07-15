7e8f1c8d81cc6cb125740c0844c36f0645ebe3cb
Feyisara Agiri <mohrenny8@gmail.com>
2025-07-15 05:36:58 +0100

    i added inline styling to my app

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+  return (
+    <footer
+      style={{
+        border: "1px solid gray",
+        backgroundColor: "navy",
+        padding: "0px 15px ",
+        color: "white",
+      }}
+    >
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,15 @@
+function MainContent() {
+  return (
+    <main
+      style={{
+        color: "navy",
+        fontWeight: "bold",
+        padding: " 3px 15px",
+        fontSize: "25px",
+      }}
+    >
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+export default MainContent;
