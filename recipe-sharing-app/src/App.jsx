abc8862fa37dfee80ae0b49a43dd96e7812d9696
Feyisara Agiri <mohrenny8@gmail.com>
2025-07-23 05:00:43 +0100

    i am working with zustand for state management

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,13 @@
+import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeList from "./components/RecipeList";
+
+function App() {
+  return (
+    <>
+      <RecipeList />
+      <AddRecipeForm />
+    </>
+  );
+}
+
+export default App;
