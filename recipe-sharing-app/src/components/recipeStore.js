f7dff70bcb2f736bf5ac3473d4462bfb1b4d9166
Feyisara Agiri <mohrenny8@gmail.com>
2025-07-23 06:14:24 +0100

    i added the add recipe button

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,23 +1,27 @@
+// src/components/recipeStore.js
 import { create } from "zustand";
 
 export const useRecipeStore = create((set) => ({
-  recipes: [
-    {
-      id: "1",
-      title: "Jollof Rice",
-      description: "Delicious West African rice.",
-    },
-    { id: "2", title: "Egusi Soup", description: "Nigerian melon seed soup." },
-  ],
+  recipes: [],
+
+  addRecipe: (newRecipe) =>
+    set((state) => ({
+      recipes: [...state.recipes, newRecipe],
+    })),
+
+  setRecipes: (recipes) => set({ recipes }),
+
   updateRecipe: (id, updatedData) =>
     set((state) => ({
       recipes: state.recipes.map((recipe) =>
         recipe.id === id ? { ...recipe, ...updatedData } : recipe
       ),
     })),
+
   deleteRecipe: (id) =>
     set((state) => ({
       recipes: state.recipes.filter((recipe) => recipe.id !== id),
     })),
 }));
+
 export default useRecipeStore;
