1b4387590419d3076b56c4052208662f779fd189
Feyisara Agiri <mohrenny8@gmail.com>
2025-07-23 07:16:32 +0100

    i made some corrections

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,5 +1,6 @@
 // RecipeList.jsx
 import { useRecipeStore } from "./recipeStore";
+import { Link } from "react-router-dom";
 
 const RecipeList = () => {
   const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);
@@ -8,8 +9,15 @@ const RecipeList = () => {
     <div>
       {filteredRecipes.length > 0 ? (
         filteredRecipes.map((recipe) => (
-          <div key={recipe.id}>
-            <h3>{recipe.title}</h3>
+          <div key={recipe.id} style={{ marginBottom: "1.5rem" }}>
+            <h3>
+              <Link
+                to={`/recipes/${recipe.id}`}
+                style={{ textDecoration: "none", color: "#2c3e50" }}
+              >
+                {recipe.title}
+              </Link>
+            </h3>
             <p>{recipe.description}</p>
           </div>
         ))
