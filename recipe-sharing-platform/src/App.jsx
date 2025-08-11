09a981ca608f86e51cc77a91634dd9d843441f81
Feyisara Agiri <mohrenny8@gmail.com>
2025-08-11 12:02:40 +0100

    I initialized a new directory

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold text-blue-500">Hello Tailwind!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
