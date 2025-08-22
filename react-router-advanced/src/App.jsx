9b91b3e9f9ae8f66929825bd999ca1566a136fc7
Feyisara Agiri <mohrenny8@gmail.com>
2025-08-22 11:42:41 +0100

    Fixed the blogpost route

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./components/Home";
 import Blog from "./components/Blog";
 import Profile from "./components/Profile";
 import PrivateRoute from "./components/PrivateRoute";
+import BlogPost from "./components/BlogPost";
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/blog/:id" element={<Blog />} />
+        <Route path="/blog/:postId" element={<BlogPost />} />
+
         <Route
           path="/profile/*"
           element={
