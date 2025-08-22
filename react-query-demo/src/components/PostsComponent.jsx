7103401827d81dc4c376a1470cdf5b0c2cf3002e
Feyisara Agiri <mohrenny8@gmail.com>
2025-08-22 06:35:49 +0100

    Fixed some errors

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 import { useQuery } from "@tanstack/react-query";
 
-const fetchData = async () => {
+const fetchPosts = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  if (!res.ok) {
+    throw new Error("Network response was not ok");
+  }
   return res.json();
 };
 
 const PostsComponent = () => {
-  const { data, error, isLoading } = useQuery({
-    queryKey: ["fetchData"],
-    queryFn: fetchData,
+  const { data, isError, isLoading } = useQuery({
+    queryKey: ["posts"],
+    queryFn: fetchPosts,
   });
 
   if (isLoading) return <div>Loading......</div>;
-  if (error) return <div>Error loading data</div>;
+  if (isError) return <div>Error loading data</div>;
 
   return (
     <div>
